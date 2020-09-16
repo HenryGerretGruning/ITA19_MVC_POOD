f6c2da72ec1e38819bdd0083fe5521fa4ceaec66
Henry <artimax32@gmail.com>
2020-09-16 10:54:03 +0300

    Tested product.py by creating ui.py file, created 3 products (milk, bread, wine) and inserted 3 products into the output.

---
+++ ui.py
@@ -0,0 +1,13 @@
+# ui.py
+
+# import classes and files
+from product import Product
+
+# create products
+bread = Product("Bread", 0.80, 10)
+milk = Product("Milk", 0.50, 50)
+wine = Product("Wine", 5.60, 5)
+# test product view
+print(bread)
+print(milk)
+print(wine)
