c956215dd9d4e044c698651d810e3eb6f155e1c3
Henry <artimax32@gmail.com>
2020-09-16 11:18:24 +0300

    Reconfigured the file to fit the shop.py file.

--- ui.py
+++ ui.py
@@ -2,12 +2,18 @@
 
 # import classes and files
 from product import Product
+from shop import Shop
 
 # create products
 bread = Product("Bread", 0.80, 10)
 milk = Product("Milk", 0.50, 50)
 wine = Product("Wine", 5.60, 5)
-# test product view
-print(bread)
-print(milk)
-print(wine)
+
+# create shop and add products to shop
+shop = Shop()
+shop.addProduct(bread)
+shop.addProduct(milk)
+shop.addProduct(wine)
+
+
+print(shop)
