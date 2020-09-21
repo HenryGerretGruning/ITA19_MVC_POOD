c2bf87a7fcf275ee02ddb52c9bf19197f34c570a
Henry <artimax32@gmail.com>
2020-09-21 15:43:56 +0300

    Made a panda list

--- view.py
+++ view.py
@@ -1,12 +1,18 @@
+import pandas as pd
+
 class View:
     # show items
     def showItems(self, items):
-        print("Shop items")
-        print("============================")
-        print("name\t|\tprice\t|\tamount")
+        #print("Shop items")
+        #print("============================")
+        #print("name\t|\tprice\t|\tamount")
+        #for item in items:
+            #print(item.getName()+"\t|\t"+
+                  #str(item.getPrice())+"\t\t|\t"+
+                  #str(item.getAmount()))
+            #print("---------------------------")
+        #print("============================")
+        df = pd.DataFrame(columns=['Name', 'Price', 'Amount'])
         for item in items:
-            print(item.getName()+"\t|\t"+
-                  str(item.getPrice())+"\t\t|\t"+
-                  str(item.getAmount()))
-            print("---------------------------")
-        print("============================")
+            df = df.append({'Name': item.getName(), 'Price': item.getPrice(), 'Amount': item.getAmount()}, ignore_index=True)
+        print(df)
